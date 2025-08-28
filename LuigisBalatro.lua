71812d9835e77a916e4ee6c10fd344f2845ba3d2
V--R <vrart1@hotmail.com>
2025-08-28 19:24:40 -0400

    Added malverk key defines and added json file to comply with modern standards.

--- LuigisBalatro.lua
+++ LuigisBalatro.lua
@@ -1,16 +1,4 @@
---- STEAMODDED HEADER
---- MOD_NAME: Luigi's Balatro
---- MOD_ID: LuigisPictureBalatro
---- PREFIX: LPB
---- MOD_AUTHOR: [Lenuilu, V--R]
---- MOD_DESCRIPTION: Balatro but Luigi has taken over Jimbo's domain! (Special thanks to Marie(maratby), marziks, and Fytos on the Balatro Discord Server for giving me ideas for the ranks and suits!)
---- LOADER_VERSION_GEQ: 1.0.0
---- VERSION: 1.0.0
---- BADGE_COLOR: 81B622
---- DEPENDENCIES: [malverk]
-
-local atlas_key = 'LPB' -- Format: PREFIX_KEY
--- See end of file for notes
+local atlas_key = 'LPB'
 local atlas_path = 'LuigisBalatro.png' -- Filename for the image in the asset folder
 local atlas_path_hc = nil -- Filename for the high-contrast version of the texture, if existing
 
@@ -19,9 +7,7 @@ local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "K
 
 local description = 'Luigi\'s Balatro' -- English-language description, also used as default
 
------------------------------------------------------------
--- You should only need to change things above this line --
------------------------------------------------------------
+SMODS.Atlas { key = 'modicon', px = 32, py = 32, path = 'modicon.png' }
 
 SMODS.Atlas{  
     key = atlas_key..'_lc',
@@ -55,16 +41,23 @@ for _, suit in ipairs(suits) do
     }
 end
 
+-- Malverk
+
 AltTexture({
     key = 'luigi-backs',
     set = 'Back',
+    keys = {'b_red','b_blue','b_yellow','b_green','b_black','b_nebula','b_abandoned','b_checkered','b_erratic',},
+    -- display_pos = 'm_stone',
     path = 'EnhancersLuigi.png',
+    original_sheet = true
 })
 
 AltTexture({
     key = 'luigi-enhanced',
     set = 'Enhanced',
     path = 'EnhancersLuigi.png',
+    keys = {'m_bonus','m_mult','m_wild','m_glass','m_steel','m_stone','m_gold','c_base'},
+    original_sheet = true
 })
 
 AltTexture({
@@ -73,28 +66,16 @@ AltTexture({
     path = 'EnhancersLuigi.png',
 })
 
-AltTexture({
-    key = 'luigi-2',
-    set = 'Seal',
-    path = 'EnhancersLuigi.png',
-})
-
 TexturePack {
     key = 'cards',
     textures = {
-        "LPB_luigi-backs", "LPB_luigi-enhanced", "LPB_luigi-seal", "LPB_luigi-2",
+        "LPB_luigi-backs", "LPB_luigi-enhanced", "LPB_luigi-seal",
     },
         loc_txt = {
         name = "Luigi's Balatro",
         text = {
-            "Changes the cards with",
+            "Changes the cards into",
             "a Luigi's Casino style."
         }
     }
 }
-
--- Notes:
-
--- The current version of Steamodded has a bug with prefixes in mods including `DeckSkin`s.
--- By manually including the prefix in the atlas' key, this should keep the mod functional
--- even after this bug is fixed.
